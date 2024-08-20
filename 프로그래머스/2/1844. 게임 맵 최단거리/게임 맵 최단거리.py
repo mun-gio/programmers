7d10bbd1e1192fd3f600c10038ba7f7df879452e
mungio <166682993+mun-gio@users.noreply.github.com>
2024-08-20 17:21:49 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.76 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -5,20 +5,24 @@ def solution(maps):
     dr = [1, 0, -1, 0]
     dc = [0, 1, 0, -1]
     visited = [[False] * m for _ in range(n)]
-    q = deque()
     
-    if maps[0][0] == 1:
-        q.append((0,0,1))
-        visited[0][0] = True
-    while q:
-        r, c, dist = q.popleft()
-        if r == n-1 and c == m-1:
-            return dist
-        for i in range(4):
-            nr = r + dr[i]
-            nc = c + dc[i]
-            if 0 <= nr < n and 0 <= nc < m and maps[r][c] == 1:
-                if not visited[nr][nc]:
-                    q.append((nr, nc, dist+1))
-                    visited[nr][nc] = True
-    return -1
+    def dfs(r, c):
+        # 초기 설정
+        q = deque()
+        # 시작점 예약
+        q.append((r,c))
+        visited[r][c] = True
+        while q:
+            # 현재 노드 방문
+            cur_r, cur_c = q.popleft()
+            for i in range(4):
+                next_r = cur_r + dr[i]
+                next_c = cur_r + dc[i]
+                if 0 <= next_r < n and 0 <= next_c < m and maps[next_r][next_c] == 1:
+                    dfs(r, c)
+    for r in range(n):
+        for c in range(m):
+            if maps[r][c] == 1 and not visited[r][c]:
+                dfs(r, c)
+                answer += 1
+    return answer
