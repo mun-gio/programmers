92deeaf62192d87151076465a093c1e650064bd3
mungio <166682993+mun-gio@users.noreply.github.com>
2024-09-10 11:35:08 +0900

    [level 0] Title: 배열 만들기 3, Time: 9.97 ms, Memory: 15.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.py
@@ -0,0 +1,6 @@
+def solution(arr, intervals):
+    answer = []
+    for i in range(len(intervals)):
+        for j in range(intervals[i][0],intervals[i][1]+1):
+            answer.append(arr[j])
+    return answer
