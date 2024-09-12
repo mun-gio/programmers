c021acbea44304e8ed09b2c32f84ac17ddd159de
mungio <166682993+mun-gio@users.noreply.github.com>
2024-09-12 10:46:42 +0900

    [level 2] Title: 카펫, Time: 0.27 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,9 +1,6 @@
 def solution(brown, yellow):
     for h in range(1,(brown+yellow)//2):
-        if (brown+yellow)%h == 0:
-            w = (brown+yellow)//h
-        else:
-            continue
+        w = (brown+yellow)//h
         if brown == (w*2) + ((h-2)*2) and yellow == ((w-2)*(h-2)):
             return [w,h]
             
