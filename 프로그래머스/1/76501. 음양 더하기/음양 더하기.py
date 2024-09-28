c24c9e7d6b43ac88db4e5c16bf4546f25d3c963e
mungio <166682993+mun-gio@users.noreply.github.com>
2024-09-28 15:51:19 +0900

    [level 1] Title: 음양 더하기, Time: 0.53 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,8 @@
+def solution(absolutes, signs):
+    answer = 0
+    for i in range(len(signs)):
+        if signs[i] == True:
+            answer += absolutes[i]
+        else:
+            answer += int('-'+str(absolutes[i]))
+    return answer
