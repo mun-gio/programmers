8c8295483ea7101dcb62e9eabdfecc587e9759ce
mungio <166682993+mun-gio@users.noreply.github.com>
2024-09-28 22:10:13 +0900

    [level 2] Title: n^2 배열 자르기, Time: 35.66 ms, Memory: 18.9 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -1,15 +1,5 @@
 def solution(n, left, right):
     a = []
-    s = [(left//n), (left%n)]
-    e = [(right//n), (right%n)]
-    while True:
-        if s[1] < n:
-            a.append(max(s[0], s[1])+1)
-            s[1] = s[1]+1
-        else:
-            s[1] = 0
-            s[0] = s[0]+1
-
-        if (s[0]*n)+s[1] == right:
-            a.append(max(s[0], s[1])+1)
-            return a
+    for i in range(left, right+1):
+        a.append(max(i//n, i%n)+1)
+    return a
