4692bfd13ac6d6e743d7f15a6a4af9a8cb6b80a5
mungio <166682993+mun-gio@users.noreply.github.com>
2024-09-28 23:49:24 +0900

    [level 1] Title: 모의고사, Time: 6.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -3,31 +3,18 @@ def solution(answers):
     n2 = [2, 1, 2, 3, 2, 4, 2, 5]
     n3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
         
-    n1n, n2n, n3n = 0,0,0
+    score = [0,0,0]
     answer = []
     
-    if len(answers) > len(n1):
-        for i in range(len(answers)-len(n1)):
-            n1.append(n1[i])
-    if len(answers) > len(n2):
-        for i in range(len(answers)-len(n2)):
-            n2.append(n2[i])
-    if len(answers) > len(n3):
-        for i in range(len(answers)-len(n3)):
-            n3.append(n3[i])
-    
-    for i in range(len(answers)):
-        if answers[i] == n1[i]:
-            n1n += 1
-        if answers[i] == n2[i]:
-            n2n += 1
-        if answers[i] == n3[i]:
-            n3n += 1
+    for idx, num in enumerate(answers):
+        if num == n1[idx%len(n1)]:
+            score[0] += 1
+        if num == n2[idx%len(n2)]:
+            score[1] += 1
+        if num == n3[idx%len(n3)]:
+            score[2] += 1
             
-    if n1n == max(n1n, n2n, n3n):
-        answer.append(1)
-    if n2n == max(n1n, n2n, n3n):
-        answer.append(2)
-    if n3n == max(n1n, n2n, n3n):
-        answer.append(3)
+    for idx, s in enumerate(score):
+        if s == max(score):
+            answer.append(idx+1)
     return answer
